8f3c8853eba13dcfa07decda59131550229107c3
HanaaWayne <hanaachouiter7@gmail.com>
2024-07-30 15:01:00 +0200

    ajout modif

--- src/pages/FormLogin.js
+++ src/pages/FormLogin.js
@@ -2,6 +2,11 @@ import React from "react";
 
 const FormLogin = () =>{
 
+    const [ email, setEmail] = React.useState("")
+    const [ mdp, setMdp] = React.useState("")
+    const navigate = useNavigate();
+  
+
     return(
         <>
         </>
